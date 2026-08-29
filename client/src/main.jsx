439112fcb298a08496a2d94537ae9dac6c56a29d
Sakshya2504 <singhkaserasakshya2504@gmail.com>
2026-08-29 22:07:26 +0530

    fix: wire patient workflow styles and routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 import './responsive.css';
-import './reassessment.css';
+import './patientWorkflow.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
